5074044d4b44f24ecd46da14de603065eae85fb5
dinguschan <owo_dinguschan@proton.me>
2025-02-28 08:45:05 -0500

    400 px tall

--- script.js
+++ script.js
@@ -918,7 +918,7 @@ function updateTabContent(url, content, tab) {
 
 <div class="content-containervv" id="browserlogsvv">
   <h3>Helios Browser Logs</h3>
-  <div id="logContainer" style="max-height: 500px; overflow-y: auto; border: 3px solid #66ffa1; padding: 10px; background: #222; color: #fff; border-radius: 5px; width: 750px;"></div>
+  <div id="logContainer" style="max-height: 400px; overflow-y: auto; border: 3px solid #66ffa1; padding: 10px; background: #222; color: #fff; border-radius: 5px; width: 750px;"></div>
 </div>
 
 
